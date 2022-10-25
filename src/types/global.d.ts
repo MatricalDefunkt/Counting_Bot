050aceb0a76b75e66d49fcb55cc24cc2b2776e34
MatricalDefunkt <pramitsmusic@gmail.com>
2022-10-25 13:24:44 +0530

    Added definitions for new methods to get configs, counts and member counts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,11 +2,7 @@
 
 import { Client, Collection, MessageCollector } from "discord.js";
 import * as globals from "global";
-import {
-	Configs,
-	Counts,
-	MemberCounts as MemberCountsDatabase,
-} from "../database/database";
+import { Configs, Counts, MemberCounts } from "../database/database";
 
 declare global {
 	/**
@@ -23,6 +19,29 @@ declare global {
 	var ServerCounts: Collection<string, Counts>;
 	/**
 	 * Collection of active message collectors
+	 * @ignore This is not implemented yet.
 	 */
 	var CountingCollectors: Collection<string, MessageCollector>;
+	/**
+	 * Global method to get the server configuration (or create if one doesn't exist) for a server.
+	 * @param {string} guildId The id of the server.
+	 * @returns {Promise<Configs>} The server configuration.
+	 */
+	function getServerConfig(guildId: string): Promise<Configs>;
+	/**
+	 * Global method to get the server count (or create if one doesn't exist) for a server.
+	 * @param {string} guildId The id of the server.
+	 * @returns {Promise<Counts>} The server count.
+	 */
+	function getServerCount(guildId: string): Promise<Counts>;
+	/**
+	 * Global method to get the count of a member in a server (or create if one doesn't exist).
+	 * @param {string} memberId The id of the member.
+	 * @param {string} guildId The id of the server.
+	 * @returns {Promise<MemberCounts>} The member count.
+	 */
+	function getMemberCount(
+		memberId: string,
+		guildId: string
+	): Promise<MemberCounts>;
 }
