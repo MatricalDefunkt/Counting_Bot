fe13108d50fcd162abc9b828b581f66e24eee356
MatricalDefunkt <pramitsmusic@gmail.com>
2022-10-30 01:32:38 +0530

    Prevented creation of new records if channel is not counting channel

--- src/events/countingHandler.ts
+++ src/events/countingHandler.ts
@@ -12,47 +12,22 @@ export class CountingHandler implements Event {
       if (message.author.bot) return;
       if (message.inGuild()) {
         const guildId = message.guildId;
-        const serverConfig = ServerConfigs.get(guildId);
-        if (!serverConfig) {
-          const serverConfig =
-            (await Configs.findByPk(guildId)) ??
-            (await Configs.create({ guildId }));
-          ServerConfigs.set(serverConfig.guildId, serverConfig);
-          console.error("Did not find server config for " + guildId);
-          return;
-        }
-        const serverCount = ServerCounts.get(guildId);
-        if (!serverCount) {
-          const serverCount =
-            (await Counts.findByPk(guildId)) ??
-            (await Counts.create({
-              guildId,
-              count: 0,
-              highestCount: 0,
-              lastMessageId: message.id,
-              lastCounterId: message.author.id,
-            }));
-          ServerCounts.set(serverCount.guildId, serverCount);
-          console.error("Did not find server count for " + guildId);
-          return;
-        }
-        let memberCount = await MemberCounts.findOne({
-          where: { guildId, userId: message.author.id },
-        });
-        if (!memberCount) {
-          memberCount =
-            (await MemberCounts.findOne({
-              where: { guildId, userId: message.author.id },
-            })) ??
-            (await MemberCounts.create({
-              guildId,
-              counts: 0,
-              lastCount: 0,
-              userId: message.author.id,
-            }));
-        }
+        const serverConfig = await getServerConfig(guildId);
 
         if (message.channelId !== serverConfig.countingChannelId) return;
+
+        const serverCount = await getServerCount(guildId, {
+          count: 0,
+          highestCount: 0,
+          lastCounterId: message.author.id,
+          lastMessageId: message.id,
+        });
+
+        const memberCount = await getMemberCount(message.author.id, guildId, {
+          count: 0,
+          lastCount: 0,
+        });
+
         const [stringNumber] = message.content.split(" ");
         if (Number(stringNumber) || Number(stringNumber) === 0) {
           const number = Number(stringNumber);
